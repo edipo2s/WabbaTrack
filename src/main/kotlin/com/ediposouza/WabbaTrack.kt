8d089a9766ad35903759d1989c0c8a17d6a2c58c
Edipo <edipo2s@gmail.com>
2017-04-29 14:24:03 -0300

    Show Matches

--- src/main/kotlin/com/ediposouza/WabbaTrack.kt
+++ src/main/kotlin/com/ediposouza/WabbaTrack.kt
@@ -1,6 +1,7 @@
 package com.ediposouza
 
 import com.ediposouza.data.Match
+import com.ediposouza.data.MatchMode
 import com.ediposouza.model.DeckClass
 import org.w3c.dom.url.URL
 import org.w3c.fetch.RequestCredentials
@@ -14,12 +15,50 @@ import kotlin.js.Json
 import kotlin.js.json
 
 var userID: String? = null
-var userMatches: List<Match>? = null
+var userMatches: Map<DeckClass, Map<DeckClass, List<Match>>>? = null
+var matchesMode: MatchMode = MatchMode.RANKED
 
 fun Main() {
     userID = URL(document.URL).searchParams.get("id")
     buildStatisticsTable()
-    userMatches = getUserMatches()
+    getUserMatches { matches ->
+        userMatches = matches
+                .groupBy { it.player.cls }
+                .mapValues {
+                    it.value.groupBy { it.opponent.cls }
+                }
+        showMatches()
+    }
+}
+
+@Suppress("UnsafeCastFromDynamic", "UNCHECKED_CAST_TO_NATIVE_INTERFACE")
+private fun getUserMatches(onSuccess: (List<Match>) -> Unit) {
+    window.fetch("https://tes-legends-assistant.firebaseio.com/users/$userID/matches.json", object : RequestInit {
+        override var method: String? = "GET"
+        override var credentials: RequestCredentials? = RequestCredentials.SAME_ORIGIN
+        override var headers: dynamic = json("Accept" to "application/json")
+
+    }).then({ response ->
+        response.json().then({ json ->
+            val keys: Array<String> = js("Object").keys(json as Json)
+            onSuccess(keys.map { Match.parse(it, json[it] as Json) })
+        })
+    })
+}
+
+private fun showMatches() {
+    with(document) {
+        DeckClass.values().forEach { playerCls ->
+            getElementById("player${playerCls.name}")?.apply {
+                DeckClass.values().forEach { opponentCls ->
+                    appendChild(createElement("th").apply {
+                        val vsMatches = userMatches?.get(playerCls)?.get(opponentCls)?.filter { it.mode == matchesMode }
+                        textContent = "${vsMatches?.count { it.win } ?: 0}/${vsMatches?.count { !it.win } ?: 0}"
+                    })
+                }
+            }
+        }
+    }
 }
 
 private fun buildStatisticsTable() {
@@ -57,22 +96,4 @@ private fun buildStatisticsTable() {
             )
         }
     }
-}
-
-@Suppress("UnsafeCastFromDynamic", "UNCHECKED_CAST_TO_NATIVE_INTERFACE")
-private fun getUserMatches(): List<Match> {
-    window.fetch("https://tes-legends-assistant.firebaseio.com/users/$userID/matches.json", object : RequestInit {
-        override var method: String? = "GET"
-        override var credentials: RequestCredentials? = RequestCredentials.SAME_ORIGIN
-        override var headers: dynamic = json("Accept" to "application/json")
-
-    }).then({ response ->
-        response.json().then({ json ->
-            val keys: Array<String> = js("Object").keys(json as Json)
-            keys.map { Match.parse(it, json[it] as Json) }.forEach {
-                console.log(it)
-            }
-        })
-    })
-    return listOf()
 }
